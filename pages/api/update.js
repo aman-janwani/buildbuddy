30834b01c6d96f21623e45a902e928899c28357f
aman-janwani <amanjanwani1486@gmail.com>
2022-08-02 00:18:13 +0530

    Bug fixing trying

--- pages/api/update.js
+++ pages/api/update.js
@@ -17,86 +17,95 @@ let aman;
 async function create(req, res) {
   const body = req.body;
   prisma.portfolio
-  .create({
-    include: {
-      projects: true,
-      work: true,
-      skills: true,
-      testimonials: true,
-      contact: true,
-      links: true,
-      user: true,
-    },
-    data: {
-      name: body.name,
-      userEmail: body.email,
-      position: body.position,
-      query: body.query,
-      backgroundColor: body.backgroundColor,
-      textColor: body.textColor,
-      image: body.image,
-      about: body.about,
-      slug: body.slug,
-      projects: {
-        create: body.projects,
+    .create({
+      include: {
+        projects: true,
+        work: true,
+        skills: true,
+        testimonials: true,
+        contact: true,
+        links: true,
+        user: true,
       },
-      work: {
-        create: body.work,
-      },
-      skills: {
-        create: body.skills,
-      },
-      testimonials: {
-        create: body.testimonials,
-      },
-      links: {
-        create: body.links,
-      },
-      contact: {
-        create: {
-          name: body.contact.name,
-          email: body.contact.email,
-          address: body.contact.address,
-          phone: body.contact.phone,
+      data: {
+        name: body.name,
+        userEmail: body.email,
+        position: body.position,
+        query: body.query,
+        backgroundColor: body.backgroundColor,
+        textColor: body.textColor,
+        image: body.image,
+        about: body.about,
+        slug: body.slug,
+        projects: {
+          create: body.projects,
+        },
+        work: {
+          create: body.work,
+        },
+        skills: {
+          create: body.skills,
+        },
+        testimonials: {
+          create: body.testimonials,
+        },
+        links: {
+          create: body.links,
+        },
+        contact: {
+          create: {
+            name: body.contact.name,
+            email: body.contact.email,
+            address: body.contact.address,
+            phone: body.contact.phone,
+          },
+        },
+        user: {
+          connect: {
+            email: body.userEmail,
+          },
         },
       },
-      user: {
-        connect: {
-          email: body.userEmail,
-        },
-      },
-    },
-  }).then((res) => {
-    console.log("new data sent");
-    aman = res;
-    console.log("aman", aman);
-  })
-  .catch((err) => {
-    console.log(err);
-    return res.status(500).json({ err, success: false });
-  });
+    })
+    .then((res) => {
+      console.log("new data sent");
+      aman = res;
+      console.log("aman", aman);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ err, success: false });
+    });
 }
-
 
 async function addPortfolio(req, res) {
   const body = req.body;
-  const deletePortfolio = prisma.portfolio
-    .delete({
-      where: {
-        userEmail: body.userEmail,
-      },
-    })
-    // .then(() => {
-    //   console.log("Portfolio deleted");
-    // })
-    .finally(() => {
-      console.log("Portfolio deleted");
-      create(req, res);
-    }).catch((err) => {
-      console.log(err);
-    })
+  try {
+    const deletePortfolio = prisma.portfolio
+      .delete({
+        where: {
+          userEmail: body.userEmail,
+        },
+      })
+      // .then(() => {
+      //   console.log("Portfolio deleted");
+      // })
+      .finally(() => {
+        console.log("Portfolio deleted");
+        create(req, res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+      return res.status(200).json(deletePortfolio, { success: true });
+
+  } catch (error) {
+    console.error("Request error", error);
+    res.status(500).json({ error, success: false });
+  }
 
   // console.log("data", body);
   // console.log("body", updated);
-  return res.status(200).json(aman, "updated", { success: true });
+  // return res.status(200).json(deletePortfolio, "updated", { success: true });
 }
