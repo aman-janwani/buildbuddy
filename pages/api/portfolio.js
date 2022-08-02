08bdef17428fbe67f6a90d2d53605b11c907ca0f
aman-janwani <amanjanwani1486@gmail.com>
2022-08-02 19:58:27 +0530

    test

--- pages/api/portfolio.js
+++ pages/api/portfolio.js
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import NextCors from 'nextjs-cors';
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
@@ -16,14 +15,6 @@ export default async function handler(req, res) {
 
 
 async function addPortfolio(req, res) {
-
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
- });
-
 
   const body = req.body;
   try {
